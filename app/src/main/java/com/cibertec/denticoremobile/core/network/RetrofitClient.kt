2fbdbaff2d0a98ed71aeb225c3edfb14e58f0aed
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-06-25 21:10:44 -0500

    chore(retrofit): ajuste de rutas en retrofit para pruebas y android manisfest

--- app/src/main/java/com/cibertec/denticoremobile/core/network/RetrofitClient.kt
+++ app/src/main/java/com/cibertec/denticoremobile/core/network/RetrofitClient.kt
@@ -15,7 +15,7 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+    private const val BASE_URL = "http://192.168.101.3:8080/api/v1/"
 
     @Volatile
     private var api: DentiCoreApi? = null
